3a388b37a95433fbf5ff0263f279e9fdc167de67
pynkvalerian <yenping.chew90@gmail.com>
2021-05-18 21:34:12 +0800

    added native-base and react-native-vector-icons

--- src/navigations/BottomTab.tsx
+++ src/navigations/BottomTab.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { HomeScreen, AddNewScreen, ProfileScreen } from '../screens';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 
 const Tab = createBottomTabNavigator();
 
@@ -9,12 +11,36 @@ const BottomTab = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="AddNew" component={AddNewScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              <FontAwesome5 name={'home'} solid style={styles.icon} />
+          }} />
+        <Tab.Screen
+          name="Add"
+          component={AddNewScreen}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              <FontAwesome5 name={'plus'} solid style={styles.icon} />
+          }} />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            tabBarIcon: ({ focused }) =>
+              <FontAwesome5 name={'user'} solid style={styles.icon} />
+          }} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 };
 
 export default BottomTab;
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 18
+  }
+})
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,43 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Container, Header, Card, CardItem, Text, H1 } from 'native-base';
 
 const HomeScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
+    <View style={styles.view}>
+      <Header />
+      <Container style={styles.container}>
+        <Card>
+          <CardItem header style={styles.textCenter}>
+            <Text>Total Balance</Text>
+          </CardItem>
+          <CardItem style={styles.textCenter}>
+            <H1>RM 4000</H1>
+          </CardItem>
+          <CardItem footer style={styles.footer}>
+            <Text>Income RM5000</Text>
+            <Text>Expenses RM1000</Text>
+          </CardItem>
+        </Card>
+      </Container>
     </View>
   );
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+  },
+  container: {
+    padding: 20,
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'space-around'
+  },
+  textCenter: {
+    justifyContent: 'center'
+  }
+});
