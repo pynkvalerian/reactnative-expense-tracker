63d1b876d5e9c1153a5640a19ed0158695423e10
pynkvalerian <yenping.chew90@gmail.com>
2021-06-02 17:09:28 +0800

    added chart on profile page

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,12 +1,63 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
+import { Container, H3 } from 'native-base';
+import { View } from 'react-native';
+import { LineChart } from "react-native-chart-kit";
 
 const ProfileScreen = () => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile!</Text>
+    <View style={styles.view}>
+      <Container style={styles.container}>
+        <H3 style={styles.title}>Monthly Transactions</H3>
+        <LineChart
+        data={{
+          labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+          datasets: [
+            {
+              data: [1000, 800, 1200, 500, 700, 1000]
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width - 50}
+        height={220}
+        yAxisLabel="RM"
+        chartConfig={{
+          backgroundColor: "#FFF",
+          backgroundGradientFrom: "#FFF",
+          backgroundGradientTo: "#FFF",
+          decimalPlaces: 0,
+          color: (opacity = 1) => `rgb(31, 185, 186, ${opacity})`,
+          labelColor: (opacity = 1) => `rgb(0, 0, 0, ${opacity})`,
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#FFF"
+          }
+        }}
+        bezier
+        fromZero
+        style={{
+          marginVertical: 8,
+          borderRadius: 10
+        }}
+      />
+      </Container>
     </View>
   );
 }
 
 export default ProfileScreen;
+
+const styles = StyleSheet.create({
+  view: {
+    flex: 1,
+    backgroundColor: '#F3F4F6'
+  },
+  container: {
+    padding: 20,
+    backgroundColor: '#F3F4F6'
+  },
+  title: {
+    padding: 20,
+  },
+});
